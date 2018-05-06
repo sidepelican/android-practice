4c5d8e35d3410da0437528970ac77c7df58e7a92
kenta_okamura <side.junktown@gmail.com>
2018-05-06 23:10:57 +0900

    add options menu

--- SaintTropez/app/src/main/java/junktown/side/gmail/com/sainttropez/MainActivity.kt
+++ SaintTropez/app/src/main/java/junktown/side/gmail/com/sainttropez/MainActivity.kt
@@ -2,6 +2,9 @@ package junktown.side.gmail.com.sainttropez
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +12,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.top -> {
+                imageView.setImageResource(R.drawable.toppage)
+                return true
+            }
+            R.id.lunch01 -> {
+                imageView.setImageResource(R.drawable.lunch01)
+                return true
+            }
+            R.id.lunch02 -> {
+                imageView.setImageResource(R.drawable.lunch02)
+                return true
+            }
+            R.id.dinner01 -> {
+                imageView.setImageResource(R.drawable.dinner01)
+                return true
+            }
+            R.id.dinner02 -> {
+                imageView.setImageResource(R.drawable.dinner02)
+                return true
+            }
+        }
+
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
